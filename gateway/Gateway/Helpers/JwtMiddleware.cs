92436f39a006f53c0d8c378ae284141b18e651dc
Aron Hemmes <aronhemmes@live.nl>
2022-12-08 13:40:43 +0100

    Update gateway auth

--- gateway/Gateway/Helpers/JwtMiddleware.cs
+++ gateway/Gateway/Helpers/JwtMiddleware.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.IdentityModel.Tokens;
 using Ocelot.Middleware;
 using System.IdentityModel.Tokens.Jwt;
+using System.Net;
 using System.Text;
 
 namespace Gateway.Helpers;
@@ -9,13 +10,17 @@ public static class JwtMiddleware
     public static Func<HttpContext, string, Func<Task>, Task> CreateAuthorizationFilter
         => async (context, securityKey, next) =>
         {
-            var routeClaimsRequirements = context.Items.DownstreamRoute().RouteClaimsRequirement;
-            if (routeClaimsRequirements.Count > 0)
+            if (bool.Parse(context.Items.DownstreamRoute().RouteClaimsRequirement["Auth"]) == true)
             {
                 var token = context.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
-                if (token != null && Validate(routeClaimsRequirements, token, securityKey))
+                if (token != null && Validate(token, securityKey))
                 {
                     await next.Invoke();
+                }
+                else
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                    context.Items.SetError(new UnauthenticatedError("Unauthorized"));
                 }
             }
             else
@@ -24,11 +29,11 @@ public static class JwtMiddleware
             }
         };
 
-    public static bool Validate(Dictionary<string, string> routeClaimsRequirements, string token, string securityKey)
+    public static bool Validate(string token, string securityKey)
     {
-        // validate token
         try
         {
+            // validate token
             var tokenHandler = new JwtSecurityTokenHandler();
             var key = Encoding.ASCII.GetBytes(securityKey);
             tokenHandler.ValidateToken(token, new TokenValidationParameters
