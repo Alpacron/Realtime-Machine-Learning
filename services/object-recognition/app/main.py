99404793800bbd0cc7b9baabc478d842b87fd2a7
Aron Hemmes <aronhemmes@live.nl>
2022-12-08 16:14:03 +0100

    Create object recognition service

---
+++ services/object-recognition/app/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
+app = FastAPI(
+    title="Object Recognition",
+    description="Valorant Object Recognition.",
+    version="1.0.0"
+)
+
+allowed_methods = ['POST', 'PUT', 'GET']
+
+app.add_middleware(
+    CORSMiddleware, allow_methods=allowed_methods, allow_headers=["*"])
+
+
+@app.get("/")
+def get_request():
+    return "Hello World!"
